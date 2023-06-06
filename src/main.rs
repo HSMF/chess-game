f45ba20fade791a2556ee08553313ff475f0ecaa
Conradin Laux <colaux@student.ethz.ch>
2023-06-06 17:04:00 +0200

    fix: fix lifetime issues with closure

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use chess_game::{graphics, Game, Ply};
 use std::io::{BufRead, Write};
 
+macro_rules! retry {
+    ($e:expr, $game:expr) => {{
+        match $e {
+            Ok(c) => {c}
+            Err(e) => {
+                eprintln!("{e}");
+                print!("{:?} make a move: ", $game.player_to_move());
+                std::io::stdout().flush()?;
+                continue;
+            }
+        }
+    }};
+}
+
 fn main() -> anyhow::Result<()> {
     enum Mode {
         Cli,
@@ -24,20 +38,8 @@ fn main() -> anyhow::Result<()> {
             std::io::stdout().flush()?;
             for line in stdin.lock().lines() {
                 let line = line?;
-                let mut parse_and_run = || {
-                    let ply = Ply::parse_san(&line, &game)?;
-                    game.try_make_move(ply)?;
-                    Ok::<_, anyhow::Error>(())
-                };
-                match parse_and_run() {
-                    Ok(..) => {}
-                    Err(e) => {
-                        eprintln!("{e}");
-                        print!("{:?} make a move: ", game.player_to_move());
-                        std::io::stdout().flush()?;
-                        continue;
-                    }
-                };
+                let ply = retry!(Ply::parse_san(&line, &game), game);
+                retry!(game.try_make_move(ply), game);
                 println!("{game}");
 
                 print!("{:?} make a move: ", game.player_to_move());
